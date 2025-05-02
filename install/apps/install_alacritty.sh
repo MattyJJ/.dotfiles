d9410f82c7d60f4ef02a824c3093e86d4d352176
MattyJJ <matthewjpendlebury@gmail.com>
2025-05-02 10:17:51 +1000

    Idk

--- install/apps/install_alacritty.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y install cmake g++ pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
-
-~/.cargo/bin/cargo install alacritty
