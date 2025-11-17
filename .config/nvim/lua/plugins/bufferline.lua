2d46b633ca32ad11877b6b00199e8ae09d9a405a
Matthew.P <81206817+MattyJJ@users.noreply.github.com>
2025-11-18 04:16:08 +1100

    random changes

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -1,29 +1,34 @@
 return {
-  'akinsho/bufferline.nvim',
-  version = "*",
-  dependencies = 'nvim-tree/nvim-web-devicons', 
+    'akinsho/bufferline.nvim',
+    version = "*",
+    dependencies = 'nvim-tree/nvim-web-devicons', 
 
-  config = function()
-    require('bufferline').setup({
-      options = {
-        number = 'ordinal',
-        separator_style = 'thin',
+    config = function()
+        require('bufferline').setup({
+            options = {
+                number = 'ordinal',
+                separator_style = 'thin',
 
-        minimum_width = 12,
+                minimum_width = 12,
 
-        show_buffer_close_icons = false,
-        show_close_icon = false,
-        always_show_bufferline = true,
-      }
-    })
+                show_buffer_close_icons = false,
+                show_close_icon = false,
+                always_show_bufferline = true,
+            }
+        })
 
-    noremap('<leader>q', function()
-      vim.cmd('bprevious')
-      vim.cmd('bdelete #')
-    end, 'Close current buffer')
+        noremap('<leader>q', function()
+            local bufs = vim.fn.getbufinfo({ buflisted = 1 })
+            if #bufs > 1 then
+                vim.cmd('bprevious')
+                vim.cmd('bdelete #')
+            else
+                vim.cmd('bdelete!')
+            end
+        end, 'Close current buffer')
 
-    for i = 1, 9 do
-      noremap('<A-' .. i .. '>', '<Cmd>BufferLineGoToBuffer ' .. i .. '<CR>')
-    end
-  end,
+        for i = 1, 9 do
+            noremap('<A-' .. i .. '>', '<Cmd>BufferLineGoToBuffer ' .. i .. '<CR>')
+        end
+    end,
 }
---
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,21 @@
+return {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    event = "VeryLazy",
+    config = function()
+        require("toggleterm").setup{
+            size = 18,
+            open_mapping = [[<C-\>]],
+            hide_numbers = true,
+            shade_filetypes = {},
+            shade_terminals = true,
+            shading_factor = 2,
+            start_in_insert = true,
+            direction = "float",
+            float_opts = {
+                border = "single",
+            }
+        }
+    end
+}
+
