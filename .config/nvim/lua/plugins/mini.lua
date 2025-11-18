0c6ee80ed356431a3fa44d092c9d52f55a93e97f
Matthew.P <81206817+MattyJJ@users.noreply.github.com>
2025-11-18 18:38:14 +1100

    Fixed a neovim keybind where if the selected line of visual was below the starting like it would not work

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,45 +1,53 @@
 return {
-  'nvim-mini/mini.nvim',
-  version = false,
-  config = function()
-    require('mini.pairs').setup()
-    require('mini.icons').setup()
-    require('mini.statusline').setup()
-    require('mini.cursorword').setup()
-    require('mini.indentscope').setup()
+    'nvim-mini/mini.nvim',
+    version = false,
+    config = function()
+        require('mini.pairs').setup()
+        require('mini.icons').setup()
+        require('mini.statusline').setup()
+        require('mini.cursorword').setup()
+        require('mini.indentscope').setup()
 
 
-    -- COMMENT
+        -- COMMENT
 
-    local comment = require('mini.comment')
-    comment.setup()
+        local comment = require('mini.comment')
+        comment.setup()
 
-    noremap('<C-c>', function()
-      comment.toggle_lines(vim.fn.line('.'), vim.fn.line('.'))
-    end, 'toggle commenting current line')
+        noremap('<C-c>', function()
+            comment.toggle_lines(vim.fn.line('.'), vim.fn.line('.'))
+        end, 'toggle commenting current line')
 
-    voremap('<C-c>', function()
-      comment.toggle_lines(vim.fn.line('v'), vim.fn.line('.'))
-    end, 'toggle commenting of the current selection')
+        voremap('<C-c>', function()
+            local start_line = vim.fn.line('v')
+            local selected_line = vim.fn.line('.')
 
-    local pick = require('mini.pick')
+            if start_line < selected_line then
+                comment.toggle_lines(start_line, selected_line)
+            else 
+                comment.toggle_lines(selected_line, start_line)
+            end
+                
+        end, 'toggle commenting of the current selection')
 
-    -- PICK
+        local pick = require('mini.pick')
 
-    pick.setup()
+        -- PICK
 
-    noremap('<leader>ft', function()
-      pick.builtin.grep_live({ cwd = vim.fn.expand('%:p:h') })
-    end)
+        pick.setup()
 
-    noremap('<leader>fp', function()
-      pick.builtin.grep_live()
-    end)
+        noremap('<leader>ft', function()
+            pick.builtin.grep_live({ cwd = vim.fn.expand('%:p:h') })
+        end)
 
-    noremap('<leader>ff', function()
-      pick.builtin.files()
-    end)
-  end,
+        noremap('<leader>fp', function()
+            pick.builtin.grep_live()
+        end)
+
+        noremap('<leader>ff', function()
+            pick.builtin.files()
+        end)
+    end,
 }
 
 
