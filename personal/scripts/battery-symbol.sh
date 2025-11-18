2aa8263bd6146078a285e61f01cc29fe05e7e904
Matthew.P <81206817+MattyJJ@users.noreply.github.com>
2025-11-19 10:27:13 +1100

    yaya

--- personal/scripts/battery-symbol.sh
+++ personal/scripts/battery-symbol.sh
@@ -1,16 +1,8 @@
 #!/bin/sh
 
 battery="/sys/class/power_supply/BAT0"
-message=""
 
-if [ -d $battery ]; then
-    status=$(cat $battery/status)
-    message="$status"
-else
-    message=""
-fi
-
-case "$message" in
+case "$(cat $battery/status)" in
   Charging)    echo '+' ;;
   Discharging) echo '-' ;;
   "Not Charging"|"Not charging") echo '/' ;;
